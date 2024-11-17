45c2c70744d6187224ca68fa8cb19a3175ae75de
halfcodz <jeongusrn@gmail.com>
2024-11-17 23:29:54 +0900

    second-commit (merge file with eunsu)

--- app/src/main/java/com/halfcodz/ctrl_project/MainActivity.java
+++ app/src/main/java/com/halfcodz/ctrl_project/MainActivity.java
@@ -2,15 +2,26 @@ package com.halfcodz.ctrl_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
 
+        Button add = (Button) findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PlusActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
